cffe67da7e10d05072e0ba19b4f0f41f51ef2f6c
Benny <bennyyuan1990@sina.com>
2017-10-03 21:24:52 +0800

    init

--- app/src/main/java/com/benny/uibind/MainActivity.java
+++ app/src/main/java/com/benny/uibind/MainActivity.java
@@ -2,8 +2,14 @@ package com.benny.uibind;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.benny.viewbind.BindView;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.activity_main_tv)
+    TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- compile/src/main/java/com/benny/viewbind/compile/AnnotationProcessor.java
+++ compile/src/main/java/com/benny/viewbind/compile/AnnotationProcessor.java
@@ -3,23 +3,35 @@ package com.benny.viewbind.compile;
 import com.benny.viewbind.BindContentView;
 import com.benny.viewbind.BindView;
 import com.benny.viewbind.OnClick;
-import java.util.HashSet;
+import com.google.auto.service.AutoService;
+
+import java.util.LinkedHashSet;
 import java.util.Set;
+
 import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.Messager;
 import javax.annotation.processing.ProcessingEnvironment;
 import javax.annotation.processing.RoundEnvironment;
 import javax.lang.model.SourceVersion;
+import javax.lang.model.element.Element;
 import javax.lang.model.element.TypeElement;
+import javax.lang.model.util.Elements;
+import javax.tools.Diagnostic;
 
+
+//AutoService自动生成META-INF/services/javax.annotation.processing.Processor文件
+@AutoService(Process.class)
 public class AnnotationProcessor extends AbstractProcessor {
+    private Messager mMessager;
+    private Elements mElementUtils;
 
     /**
-     * 处理支持的注解类型
+     *
      * @return
      */
     @Override
     public Set<String> getSupportedAnnotationTypes() {
-        Set<String> result = new HashSet<>();
+        Set<String> result = new LinkedHashSet<>();
         result.add(BindView.class.getCanonicalName());
         result.add(BindContentView.class.getCanonicalName());
         result.add(OnClick.class.getCanonicalName());
@@ -28,16 +40,33 @@ public class AnnotationProcessor extends AbstractProcessor {
 
     @Override
     public SourceVersion getSupportedSourceVersion() {
-        return super.getSupportedSourceVersion();
+        return SourceVersion.latestSupported();
     }
 
     @Override
     public synchronized void init(ProcessingEnvironment processingEnv) {
         super.init(processingEnv);
+        mMessager = processingEnv.getMessager();
     }
 
     @Override
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+        Set<? extends Element> bindViewElements = roundEnv.getElementsAnnotatedWith(BindView.class);
+
+        for (Element element: bindViewElements) {
+            logNote(element.toString());
+        }
         return false;
     }
+
+
+    private void logNote(String msg) {
+        mMessager.printMessage(Diagnostic.Kind.NOTE, msg);
+    }
+
+    private void logNote(String format, Object... args) {
+        mMessager.printMessage(Diagnostic.Kind.NOTE, String.format(format, args));
+    }
+
+
 }
